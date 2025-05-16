f5b80ee0eb1a43529541cf3afc2ccdc891a5e632
ValerioPalagi <132820947+ValerioPalagi@users.noreply.github.com>
2025-05-16 15:08:06 +0200

    ciao

--- Replication.jl
+++ Replication.jl
@@ -1,7 +1,13 @@
 f =10
+<<<<<<< HEAD
+println(f+5)
+
+=======
 ciao = 10
 vale_ciao=27
 print(vale_ciao)
 print(f)
 print(ciao)
 isotta_è_bella =2
+g = 20
+>>>>>>> b844ec175c6068bac1e72724ce33abd955c12689
